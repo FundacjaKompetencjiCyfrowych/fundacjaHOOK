f7114f942a632b23ef163cd4f83e60541018edf8
Brzana <szymonbrzana@gmail.com>
2026-04-26 19:11:57 +0200

    feat: created UtilityHeader.tsx

---
+++ apps/web/app/_components/UtilityHeader.tsx
@@ -0,0 +1,33 @@
+//TODO: make krs fetched from sanity and add real links to social media
+
+export default function UtilityHeader() {
+  return (
+    <header className="w-full border-b border-subtle bg-sunken py-2 px-6">
+      <div className=" mx-auto flex items-center justify-between text-xs sm:text-sm">
+        <div className="text-main">
+          KRS: <span className="font-mono">[0000000000]</span>
+        </div>
+
+        <div className="flex items-center gap-2">
+          {[
+            ["f", "#"],
+            ["in", "#"],
+            ["ig", "#"],
+          ].map(([icon, link]) => (
+            <a
+              key={icon}
+              href={link}
+              className="w-8 h-8 flex items-center justify-center bg-elevated rounded hover:bg-brand-soft transition-colors"
+            >
+              <span className="text-main lowercase">{icon}</span>
+            </a>
+          ))}
+
+          <button className="ml-2 px-4 py-1.5 bg-elevated text-main font-medium rounded-md cursor-pointer hover:bg-brand-primary hover:text-white transition-all">
+            Wesprzyj nas
+          </button>
+        </div>
+      </div>
+    </header>
+  );
+}
--- apps/web/app/page.tsx
+++ apps/web/app/page.tsx
@@ -2,6 +2,7 @@ import { q } from "@/sanity/groqd";
 import { sanityFetch } from "@/sanity/live";
 import { SanitySections } from "@/sanity/sections/SanitySections";
 import { notFound } from "next/navigation";
+import UtilityHeader from "@/app/_components/UtilityHeader";
 
 export default async function Home() {
   const home = q.star.filterByType("home").slice(0);
@@ -10,13 +11,5 @@ export default async function Home() {
   if (!data || data.length === 0) notFound();
   const h = data[0];
 
-  return (
-    <div className="flex min-h-screen items-center justify-center">
-      <main className="flex min-h-screen w-full max-w-3xl flex-col items-center justify-between py-32 px-16 sm:items-start">
-        <div className="flex flex-col items-center gap-6 text-center sm:items-start sm:text-left">
-          <SanitySections value={h?.sections} />
-        </div>
-      </main>
-    </div>
-  );
+  return <UtilityHeader />;
 }
