278599cc1279d58fc2241a59e3c5fc6df3ddcef9
Brzana <szymonbrzana@gmail.com>
2026-05-29 19:37:42 +0200

    feat: added cache to materials fetch with ttl set to days

--- apps/web/app/(root)/materials/page.tsx
+++ apps/web/app/(root)/materials/page.tsx
@@ -1,11 +1,19 @@
 import MaterialyFilters from "@/app/_components/Filtering/MaterialyFilters";
 import { mapMaterialsToFilterItems } from "@/lib/mappers/materials";
+import { cacheLife } from "next/cache";
 import { sanityFetch } from "@/sanity/live";
 import { materialsQuery } from "@/sanity/queries/materials";
 
-const MaterialsPage = async () => {
+async function getCachedMaterials() {
+  "use cache";
+  cacheLife("days");
+
   const { data } = await sanityFetch({ query: materialsQuery });
-  const materials = mapMaterialsToFilterItems(data ?? []);
+  return mapMaterialsToFilterItems(data ?? []);
+}
+
+const MaterialsPage = async () => {
+  const materials = await getCachedMaterials();
 
   return <MaterialyFilters materials={materials} />;
 };
