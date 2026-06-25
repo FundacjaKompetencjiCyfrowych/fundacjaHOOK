1efd3fffe637b602664e25fb6cd607907d8bc0f8
Brzana <szymonbrzana@gmail.com>
2026-06-25 12:24:10 +0200

    feat: added button for showing more or less projects

--- apps/web/app/(root)/projects/page.client.tsx
+++ apps/web/app/(root)/projects/page.client.tsx
@@ -17,10 +17,22 @@ const ProjectPageClient = ({
   const [sortBy, setSortBy] = useState<string>("Najnowsze");
 
   return (
-    <div className="flex sm:flex-row flex-col justify-between sm:items-center gap-4 mb-6">
-      <ProjectFilters counts={counts} filter={filter} setFilter={setFilter} />
-      <SortByOrder sort={sortBy} setSort={setSortBy} />
-    </div>
+    <>
+      <div className="flex sm:flex-row flex-col justify-between sm:items-center gap-4 mb-6">
+        <ProjectFilters counts={counts} filter={filter} setFilter={setFilter} />
+        <SortByOrder sort={sortBy} setSort={setSortBy} />
+      </div>
+      {projects.length > 3 && (
+        <div className="flex justify-center mt-8">
+          <button
+            onClick={() => setShowAllProjects(!showAllProjects)}
+            className="bg-brand-primary hover:bg-brand-onhover px-6 py-2 rounded-lg font-medium text-white transition-colors"
+          >
+            {showAllProjects ? "Zobacz mniej" : "Załaduj więcej"}
+          </button>
+        </div>
+      )}
+    </>
   );
 };
 
